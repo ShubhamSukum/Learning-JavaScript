a32052fb01a48fe481e22ad51bdcb3b8b469b386
Shubham Sukum <shubhamsukum1722002@gmail.com>
2023-03-04 20:41:18 +0530

    04/03/2023 commit

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ console.log(obj["na"+e]);
 
 const inp=prompt("Enter name,website,students,placement or library for info");
 console.log(obj[inp]);
-*/
 
 const obj={
     name1:"PICT",
@@ -22,14 +21,63 @@ const obj={
     placement:true,
     average:98,
     Library:"OKAY",
-    percent:function(){
-        if(this.average>=97){
-            return `Eligible in ${this.name1}`;
-        }
-        else{
-            return `Beta next time`;
-        }
+
+    percent:function (){
+    return `${this.name1} is the name \n${this.website} is the website \n${this.students} is the numbers of students\n${this.placement} is the placement state\n${this.average} is the average\n${this.Library} is the average\n`;
     }
+
+    // percent:function(){
+    //     if(this.average>=97){
+    //         return `Eligible in ${this.name1}`;
+    //     }
+    //     else{
+    //         return `Beta next time`;
+    //     }
+    // }
+
+    // Didn't Worked!!
+    // percent:function(){      
+    //     this.name1=`Eligible in ${this.name1}`;
+    //     console.log(name1);
+    // }
 };
 
 console.log(obj.percent());
+*/
+
+/* Assignment Implemented
+
+const mark={
+    firstName:"Mark",
+    mass:78.9,
+    height:1.7,
+    calcBmi:function(){
+        return this.mass/this.height**2;
+    }
+};
+
+const john={
+    firstName:"John",
+    mass:80.9,
+    height:1.6,
+    calcBmi:function(){
+        return this.mass/this.height**2;
+    }
+};
+
+console.log(john.calcBmi(),mark.calcBmi());
+
+const bmi1=john.calcBmi();
+const bmi2=mark.calcBmi()
+
+    if(bmi1>bmi2)
+    {
+        console.log(`${john.firstName}'s BMI(${bmi1}) is higher than ${mark.firstName}'s BMI(${bmi2})`);
+    }
+    else if(bmi1<bmi2)
+    {
+        console.log(`${mark.firstName}'s BMI(${bmi2}) is higher than ${john.firstName}'s BMI(${bmi1})`);
+    }
+
+*/
+
