3613f8ef7c4d689079914ef0990952f0607cf2ca
Shubham Sukum <shubhamsukum1722002@gmail.com>
2023-03-04 18:05:42 +0530

    objects methods

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 /*
 objects
-*/
 
-const obj={
-    name:"PICT",
-    website:"www.pict.edu",
-    students:840,
-    placement:true,
-    Library:"OKAY"
-};
 
 const e="me";
 console.log(obj.placement);
@@ -18,3 +10,26 @@ var size = Object.keys(obj).length;
 console.log(size);
 
 console.log(obj["na"+e]);
+
+const inp=prompt("Enter name,website,students,placement or library for info");
+console.log(obj[inp]);
+*/
+
+const obj={
+    name1:"PICT",
+    website:"www.pict.edu",
+    students:840,
+    placement:true,
+    average:98,
+    Library:"OKAY",
+    percent:function(){
+        if(this.average>=97){
+            return `Eligible in ${this.name1}`;
+        }
+        else{
+            return `Beta next time`;
+        }
+    }
+};
+
+console.log(obj.percent());
