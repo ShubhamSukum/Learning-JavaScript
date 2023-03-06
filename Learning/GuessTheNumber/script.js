882faa84b31ab0e7613a0487fbae4997d4e90b49
Shubham Sukum <shubhamsukum1722002@gmail.com>
2023-03-06 19:29:44 +0530

    DOM guess game!

--- Learning/GuessTheNumber/script.js
+++ Learning/GuessTheNumber/script.js
@@ -9,18 +9,71 @@
 const NO=Math.trunc(Math.random()*20+1);
 document.querySelector(".number").textContent=NO;
 
+let score=5;
+document.querySelector(".score").textContent=5;
+
 document.querySelector(".check").
 addEventListener('click',function(){
+
     const num=Number(document.querySelector(".guess").value);
     console.log(num);
 
     if(!num){
         document.querySelector(".message").textContent="No Input!";
-    }else if(num===NO){
-        document.querySelector(".message").textContent="Correct Answer!";
-    }else if(num>NO){
-        document.querySelector(".message").textContent="HIGH!!";
-    }else if(num<NO){
-        document.querySelector(".message").textContent="LOW!!";
+    }
+
+    else if(num===NO){
+            // Styling
+            document.querySelector("body").
+            style.backgroundColor="green";
+            
+            document.querySelector(".badge").style.fontSize="2.3rem";
+            
+            document.querySelector(".message").
+            textContent="Correct Answer!";
+
+            score+=20;
+            console.log(document.querySelector(".score").
+            textContent=score);
+    }
+    
+    else if(num>NO){
+
+        if(score>1)
+        {
+            document.querySelector(".message").
+            textContent="HIGH!!";
+
+            score--;
+            console.log(document.querySelector(".score").
+            textContent=score);
+        }
+        else{
+            document.querySelector(".message").
+            textContent="Game Over!!";
+
+            document.querySelector(".score").
+            textContent=0;
+        }
+        
+    }
+    
+    else if(num<NO){
+        if(score>1)
+        {
+            document.querySelector(".message").
+            textContent="LOW!!";
+
+            score--;
+            console.log(document.querySelector(".score").
+            textContent=score);
+        }
+        else{
+            document.querySelector(".message").
+            textContent="Game Over!!";
+
+            document.querySelector(".score").
+            textContent=0;
+        }
     }
 });
