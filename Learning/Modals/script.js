4ab8ac18f1bcd089faec81b23eac09ab7086a7c4
Shubham Sukum <shubhamsukum1722002@gmail.com>
2023-03-06 23:46:48 +0530

    06/03/2023

--- Learning/Modals/script.js
+++ Learning/Modals/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const butts=document.querySelectorAll(".show-modal");
+const modal=document.querySelector(".modal");
+const butCloseModal=document.querySelector(".close-modal");
+const overlays=document.querySelector(".overlay");
+
+for(let i=0;i<butts.length;i++)
+{
+    console.log(butts[i].textContent);
+}
