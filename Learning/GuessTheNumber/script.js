bd8f7a3e8475f114b0fc5dc3ed4e01bdb0f608e9
Shubham Sukum <shubhamsukum1722002@gmail.com>
2023-03-06 18:03:47 +0530

    Guessing Game!!

--- Learning/GuessTheNumber/script.js
+++ Learning/GuessTheNumber/script.js
@@ -1,5 +1,26 @@
 'use strict';
 
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
 
+// console.log(document.querySelector(".label-highscore").textContent);
+// while(true)
+// console.log(document.querySelector(".guess").value);
 
+const NO=Math.trunc(Math.random()*20+1);
+document.querySelector(".number").textContent=NO;
+
+document.querySelector(".check").
+addEventListener('click',function(){
+    const num=Number(document.querySelector(".guess").value);
+    console.log(num);
+
+    if(!num){
+        document.querySelector(".message").textContent="No Input!";
+    }else if(num===NO){
+        document.querySelector(".message").textContent="Correct Answer!";
+    }else if(num>NO){
+        document.querySelector(".message").textContent="HIGH!!";
+    }else if(num<NO){
+        document.querySelector(".message").textContent="LOW!!";
+    }
+});
