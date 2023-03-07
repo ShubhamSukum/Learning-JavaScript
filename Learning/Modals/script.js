3bc6a0092c6657acb8c4ba4313fdb709b88b3c0e
Shubham Sukum <shubhamsukum1722002@gmail.com>
2023-03-07 16:05:51 +0530

    Final Push

--- Learning/Modals/script.js
+++ Learning/Modals/script.js
@@ -5,7 +5,27 @@ const modal=document.querySelector(".modal");
 const butCloseModal=document.querySelector(".close-modal");
 const overlays=document.querySelector(".overlay");
 
+const openModal=function(){
+    console.log("Button CLicked!!");
+    modal.classList.remove("hidden");
+    overlays.classList.remove("hidden");
+    // modal.style.display="block";
+};
+
+const funcDRY=function(){
+    modal.classList.add("hidden");
+    overlays.classList.add("hidden");
+    console.log("Button Closed🤫");
+};
+
 for(let i=0;i<butts.length;i++)
-{
-    console.log(butts[i].textContent);
-}
+butts[i].addEventListener("click",openModal);
+
+butCloseModal.addEventListener("click",funcDRY);
+overlays.addEventListener("click",funcDRY);
+
+document.addEventListener("keypress",function(event){
+    if(event.key==="-" && !modal.classList.contains("hidden")){
+        funcDRY();
+    }
+});
