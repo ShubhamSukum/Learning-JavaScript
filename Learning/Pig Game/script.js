a0e2327ac997c2e18e3eae890022984c97152b79
Shubham Sukum <shubhamsukum1722002@gmail.com>
2023-03-08 17:40:27 +0530

    pig game almost done!!

--- Learning/Pig Game/script.js
+++ Learning/Pig Game/script.js
@@ -11,8 +11,20 @@ const butNew=document.querySelector(".btn--new");
 const butRoll=document.querySelector(".btn--roll");
 const butHold=document.querySelector(".btn--hold");
 
+let play=true; 
+
 score0El.textContent=0;
 score1El.textContent=0;
+
+const switch1=function(){
+    document.getElementById(`current--${active}`).
+        textContent=0;
+        curScore=0;
+        active=active===0?1:0;
+
+        player0El.classList.toggle("player--active");
+        player1El.classList.toggle("player--active");
+};
 
 let scores=[0,0];
 let curScore=0;
@@ -21,27 +33,51 @@ let active=0;
 diceEl.classList.add("hidden");
 
 butRoll.addEventListener("click",function(){
-    // generating a random number
-    const diceNo=Math.trunc(Math.random()*6)+1;
-    console.log(diceNo);
 
-    // displaying the changed no. as per random val
-    diceEl.src=`dice-${diceNo}.png`;
+    if(play)
+    {   
+        // generating a random number
+        const diceNo=Math.trunc(Math.random()*6)+1;
+        // console.log(diceNo);
 
-    diceEl.classList.remove("hidden");
+        // displaying the changed no. as per random val
+        diceEl.src=`dice-${diceNo}.png`;
 
-    if(diceNo!==1)
-    {
-        curScore+=diceNo;
+        diceEl.classList.remove("hidden");
 
-        document.getElementById(`current--${active}`).
-        textContent=curScore;
+        if(diceNo!==1)
+        {
+            curScore+=diceNo;
+
+            document.getElementById(`current--${active}`).
+            textContent=curScore;
+        }
+        else
+        {
+            switch1();
+        }
     }
-    else
-    {
-        document.getElementById(`current--${active}`).
-        textContent=0;
-        curScore=0;
-        active=active===0?1:0;
+    
+    
+});
+
+butHold.addEventListener("click",function(){
+    if(play){
+        scores[active]+=curScore;
+
+        document.getElementById(`score--${active}`).
+        textContent=scores[active];
+
+        if(scores[active]>=10){
+            play=false;
+            diceEl.classList.add("hidden");
+            document.querySelector(`.player--${active}`).
+            classList.add("player--winner");
+
+            document.querySelector(`.player--${active}`).
+            classList.remove("player--active");
+        }
+
+        switch1();
     }
 });
