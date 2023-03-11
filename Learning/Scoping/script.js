8e23160973e00cc47ba3a31ba8c0b5d32e9394bb
Shubham Sukum <shubhamsukum1722002@gmail.com>
2023-03-11 21:00:12 +0530

    ugrag *u*

--- Learning/Scoping/script.js
+++ Learning/Scoping/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const nameFirst="Shubham";
+const rollNo=71;
+
+function personal(nameFirst){
+    let no=rollNo-33;
+    console.log(`The between is ${no} ... is of ${nameFirst}`);
+    /*
+    console.log(`In outter function ${nammmm}`);
+    
+    script.js:19 Uncaught ReferenceError: nammmm is not defined
+    at personal (script.js:19:39)
+    at script.js:24:1
+    */
+}
+
+
+var S=function nick(){
+    if(nameFirst==="Shubham"){
+        var nammmm="shubz";
+        // let nammmm="shubz";
+
+        console.log(`Real name ${nameFirst} but nickname is ${nammmm} in *******IF BLOCK****`);
+    }
+    console.log(`In Function block var is working => ${nammmm}`);
+    /* 
+        IF used let instead of var
+        let nammmm="shubz";
+
+        script.js:24 Uncaught ReferenceError: nammmm is not defined
+        at nick (script.js:24:56)
+        at script.js:26:1
+    */
+}
+
+S();
+personal("Shubham");
