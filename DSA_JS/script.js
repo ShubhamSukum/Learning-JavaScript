b06e797f5667d8813c2cfc283c7d0f266bdaf3e3
Shubham Sukum <shubhamsukum1722002@gmail.com>
2023-03-16 16:13:23 +0530

    destructuring

--- DSA_JS/script.js
+++ DSA_JS/script.js
@@ -28,17 +28,10 @@ const restaurant = {
   },
 };
 
-
-
-
-
+/*
+  DESTRUCTURING
+*/
 
 const arr=[2,3,4,5];
-
 const [a,b]=arr;
-
 console.log(a,b);
-
-const shubz=new Array(1,2,4,4,2); //new array(1,0,0,0) gives error
-
-console.log(shubz);
