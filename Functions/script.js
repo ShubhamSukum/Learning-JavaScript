b873c9c523696d6aa073d02bf9dd938d9d53ad2a
Shubham Sukum <shubhamsukum1722002@gmail.com>
2023-03-19 21:39:09 +0530

    Starting Functions

---
+++ Functions/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let Arr=[];
+
+function shubz(cns=40,cc=40,wadl=40,dsbda=40){
+    const obj={
+        cns,
+        cc,
+        wadl,
+        dsbda
+    };
+
+    Arr.push(obj);
+
+    console.log(obj);
+}
+
+shubz(58,60,57,58);
