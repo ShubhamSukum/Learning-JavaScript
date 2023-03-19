b2ed6f2bf13c59bdd2a1b9d737c31c77cdb24dfc
Shubham Sukum <shubhamsukum1722002@gmail.com>
2023-03-19 22:20:10 +0530

    19/03/2023 end!!

--- Functions/script.js
+++ Functions/script.js
@@ -1,18 +1,40 @@
 'use strict';
 
-let Arr=[];
+// let Arr=[];
 
-function shubz(cns=40,cc=40,wadl=40,dsbda=40){
-    const obj={
-        cns,
-        cc,
-        wadl,
-        dsbda
-    };
+// function shubz(cns=40,cc=40,wadl=40,dsbda=40){
+//     const obj={
+//         cns,
+//         cc,
+//         wadl,
+//         dsbda
+//     };
 
-    Arr.push(obj);
+//     Arr.push(obj);
 
-    console.log(obj);
+//     console.log(obj);
+// }
+
+// shubz(58,58);
+// shubz(70,70,70,38);
+
+// Procrastination 1-0
+
+const first=function(str){
+    return str.replace(/ /g,"").toUpperCase();
 }
 
-shubz(58,60,57,58);
+const second=function(str,fn){
+    const [first,...others]=str.split(" ");
+    return [first.toUpperCase(),...others].join(" ");
+};
+
+const trans=function(str,fn){
+    console.log(`Original string => ${str}`);
+
+    console.log(`Maniplulated => ${fn(str)}`);
+    console.log(`${fn.name} function`)
+}
+
+trans("Javascript mastery",first);
+trans("Javascript mastery",second);
